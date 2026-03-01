1f75af3aced45b828b7619071fb1d9a144a8b6ab
Tokosama <anouartoko@gmail.com>
2026-03-01 18:49:48 +0100

    bug correction

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode, Suspense } from "react";
+import Spinner from "./components/Spinner.jsx";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "lenis/dist/lenis.css";
 import App from "./App.jsx";
 import "./i18n.js";
-import Spinner from "./components/spinner.jsx";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Suspense fallback={<Spinner />}>
